521f15fc7ed4b20d330496b2bef407c88ba7c3c1
Vandemar <lokavarapuh@gmail.com>
2014-06-27 18:04:18 -0500

    Created a module with subroutine calls to sgemm and dgemm

---
+++ src/Cuda_libraries/matrixVectorMultiply.f90
@@ -0,0 +1,59 @@
+! Author: Harsha Lokavarapu
+
+        module matrix_vector_multiply
+!
+!>  kind parameter for integer type variable (  4 byte)
+      integer, parameter   ::  kint  =       4
+!>  kind parameter for real    type variable (  8 byte )
+      integer, parameter   ::  kreal =       8
+
+        implicit none
+
+!
+!----------------------------------------------------------------
+!
+        contains
+!
+!---------------------------------------------------------------
+!
+        subroutine double_matrix_vector_multiply(A, x, y, n)       
+!  Fortran90 program to multiply matrix and vector using dgemv
+!   y = alpha*A*x + beta*y
+
+! Define
+        real(kind=kreal), dimension(:,:), intent(in) ::    A
+        real(kind=kreal), dimension(:), intent(in) ::    x
+        real(kind=kreal), dimension(:), allocatable, intent(out) ::    y
+        real(kind=kreal) :: alpha=1, beta=0
+        integer, intent(in) :: n
+
+        allocate(y(n))
+        y=0
+  
+        ! Execute cublas function
+        call CUBLAS_DGEMV('n',n,n,alpha,A,n,x,1,beta,y,1)   
+
+        end subroutine double_matrix_vector_multiply
+
+! Fortran90 program to multiply matrix and vector using Sgemv
+!   y = alpha*A*x + beta*y
+! Single precision 
+
+        subroutine single_matrix_vector_multiply(A, x, y, n)
+
+! Define
+        real(kind=kint), dimension(:,:), intent(in) ::    A
+        real(kind=kint), dimension(:), intent(in) ::    x
+        real(kind=kint), dimension(:), allocatable, intent(out) ::    y
+        real(kind=kint) :: alpha=1, beta=0
+        integer, intent(in) :: n
+
+        allocate(y(n))
+        y=0
+
+! Execute cublas function
+        call cublas_SGEMV('n',n,n,alpha,A,n,x,1,beta,y,1)   
+
+        end subroutine single_matrix_vector_multiply
+
+end module matrix_vector_multiply
